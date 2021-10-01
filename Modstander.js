4b795e0c44c120b4e140cd8627fbb5ac256e7abe
Mads505m <onlinebigchungus@gmail.com>
2021-10-01 09:33:46 +0200

    Update Modstander.js

--- Modstander.js
+++ Modstander.js
@@ -4,5 +4,10 @@ constructor(x,y, bredde,dybde,hastighed){
   this.bredde=bredde;
   this.dybde=dybde;
   this.hastighed=hastighed;
+this.speedY=5
 
+this.newPos = function() {
+       this.x += this.speedX;
+       this.y += this.speedY;
+   }  
 };
