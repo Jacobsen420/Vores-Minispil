9fcb63a37d7006103cb6f48971deea36b48593ea
Mads505m <onlinebigchungus@gmail.com>
2021-10-04 11:37:48 +0200

    Modstander start

--- Java.js
+++ Java.js
@@ -18,7 +18,7 @@ class boks {
 
   }
   draw(){
-    
+
 
     let ctx = canvas.getContext("2d");
     ctx.fillStyle = "#FF0000";
@@ -27,7 +27,7 @@ class boks {
   }
 
   move(e) {
-  
+
     console.log(e.keyCode)
 
     if (e.keyCode == 37) {
@@ -42,7 +42,7 @@ class boks {
     if (this.x < -10){
       this.x = 1830;
     }
-    
+
     if (this.x > 1830){
       this.x = -10;
     }
@@ -55,24 +55,24 @@ class boks {
 
 newspeed=xspeed;
    firkant= new boks(700, 650, 100, 100, 30);
-  
+
    document.addEventListener('keydown', function(event) {
     firkant.move(event);
 
   }
   )
-  
 
- 
+
+
 
 function loop() {
   let ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   firkant.draw();
+  modstander.draw();
 
 
   //firkant.move();
-  
+
 }
 setInterval(loop, 0)
-
--- Modstander.js
+++ Modstander.js
@@ -1,13 +1,17 @@
-constructor(x,y, bredde,dybde,hastighed){
+
+let modstander;
+class fjende{
+  constructor(x,y, bredde,dybde,hastighed){
   this.x=x;
   this.y=y;
   this.bredde=bredde;
   this.dybde=dybde;
   this.hastighed=hastighed;
-this.speedY=5
-
-this.newPos = function() {
-       this.x += this.speedX;
-       this.y += this.speedY;
-   }  
-};
+}
+draw(){
+  let ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(this.x,this.y,this.bredde,this.dybde);
+}
+}
+ modstander= new fjende(500, 200, 100, 100, 30);
