2aabbe33d4c7a7f2418e53223e9a21c24b862abd
Mads505m <onlinebigchungus@gmail.com>
2021-10-11 12:20:11 +0200

    Lige hurtigt

--- Modstander.js
+++ Modstander.js
@@ -7,11 +7,14 @@ class fjende{
   this.bredde=bredde;
   this.dybde=dybde;
   this.hastighed=hastighed;
+
 }
 draw(){
   let ctx = canvas.getContext("2d");
   ctx.fillStyle = "#FF0000";
   ctx.fillRect(this.x,this.y,this.bredde,this.dybde);
 }
-}
+
+ }
+
  modstander= new fjende(500, 200, 100, 100, 30);
